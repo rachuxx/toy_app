0adfcec765c26e6c06677b47a30f799d290257ca
rachuxx <raszkiewicz.m@gmail.com>
2016-11-23 20:49:41 +0100

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+                    presence: true
+  validates :name, presence: true
 end
